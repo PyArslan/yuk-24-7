fd60a508bbf01d8bef2e889148d060e5a3c8f24b
PyArslan <125560955+PyArslan@users.noreply.github.com>
2023-11-13 17:50:27 +0300

    Update mail.php

--- mailer/mail.php
+++ mailer/mail.php
@@ -27,19 +27,66 @@ require __DIR__.'/SMTP.php';
 
 
 
+function mail_send($half, $file, $mailfrom, $password, $server, $port)
+{
+    foreach ($half as $email)
+    {
+        $id = mb_convert_encoding($file[0], "UTF-8", "WINDOWS-1251");
+        $subject = "Новая заявка по грузоперевозкам $id";
+
+        // Формируется текст письма
+        $message = "";
+        foreach ($file as $value) {$message .= mb_convert_encoding($value, "UTF-8", "WINDOWS-1251")."\n\n";}
+        
+        // Настройка PHPMailer и SMTP
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->CharSet = "utf-8";
+        $mail->Host = $server;
+        $mail->Port = $port;
+        $mail->SMTPAuth = true;
+        $mail->Username = $mailfrom;
+        $mail->Password = $password;
+        $mail->SMTPSecure = 'tsl';
+
+        $mail->From = $mailfrom;
+        $mail->FromName = $mailfrom;
+
+        $mail->AddAddress(strtolower(trim($email)), "");
+        $mail->AddReplyTo($mailfrom);
+
+        $mail->Body = stripslashes($message);
+        $mail->Subject = stripslashes($subject);
+        $mail->WordWrap = 80;
+
+        if (!$mail->Send())
+        {
+            // Запись в лог файл сведения об ошибке
+            $log = fopen("log.txt","a");
+            fwrite($log, iconv("UTF-8", "WINDOWS-1251", "\n\n1 часть - Ошибка при отправке $email, \n\n".$mail->ErrorInfo.",  \n\n$message"));
+            fclose($log);
+            continue;
+        } else {sleep(2);}
+        
+    }// for each emails
+}
+
+
 // создаём переменную с функцией открытия проверочного файла
 $check = fopen("check.txt","r") or die(0);
 
 // если в проверочном  файле значение OFF то начинается след. проверка
-if(fread($check, filesize("check.txt")) == "OFF"){
+if(fread($check, filesize("check.txt")) == "OFF")
+{
 	fclose($check);
-
 	$files_array = array();
 
 	// сканирование директории и поиск файлов с заявкой для рассылки
 	$dir = scandir(getcwd());
-	foreach ($dir as $file) {
-	    if(strpos($file, "ew_application")){
+	foreach ($dir as $file)
+    {
+	    if(strpos($file, "ew_application"))
+        {
 	        array_push($files_array, $file);
 	        break;
 	    }
@@ -47,7 +94,8 @@ if(fread($check, filesize("check.txt")) == "OFF"){
 
 
 	// если массив в файлами не пустой то начинается выполнение рассылки
-	if (!empty($files_array)){
+	if (!empty($files_array))
+    {
 		// меняется статус в проверочном файле (чтобы следующий запуск этого скрипта не создал дубликат задачи а просто свернулся)
 		$check = fopen("check.txt","w");
 		fwrite($check, "ON");
@@ -62,16 +110,12 @@ if(fread($check, filesize("check.txt")) == "OFF"){
 
 	    // ----------- SQL -------------- \\
 		
-	    $servername = "127.0.0.1:3306";
-	    $username = "root";
-	    $password = "";
-	    $dbname = "haytek_gruz247";
-
 	    $emails_array = array();
 
-	    $conn = new mysqli($servername, $username, $password, $dbname);
+	    $conn = new mysqli("SERVER", "USER", "PASSWORD", "DATABASE");
 
-	    if ($conn->connect_error) {
+	    if ($conn->connect_error)
+        {
 	    	$log = fopen("log.txt","a");
 			fwrite($log, iconv("UTF-8", "WINDOWS-1251", "\n\nConnection failed: ".$conn->connect_error));
 			fclose($log);
@@ -81,129 +125,39 @@ if(fread($check, filesize("check.txt")) == "OFF"){
 	    $sql = "SELECT Email FROM Klienty WHERE Email != '' ORDER BY ID";
 
 	    $result = $conn->query($sql);
-	    if ($result->num_rows > 0) {
-	     
-	        while($row = $result->fetch_assoc()) {
+
+	    if ($result->num_rows > 0) 
+        {
+	        while($row = $result->fetch_assoc()) 
+            {
 	            array_push($emails_array, $row['Email']);
 	        }
 	    }
 		
-		$len = (int) count($emails_array);
-
-		$firsthalf = array_slice($emails_array, 0, $len / 2);
-		$secondhalf = array_slice($emails_array, $len / 2);
+        $emails_array = array_chunk($emails_array, ceil(count($emails_array)/3));
+		$firsthalf = $emails_array[0];
+		$secondhalf = $emails_array[1];
+        $thirdhalf = $emails_array[2];
 
 	    // ------------- Sending ------------- \\
 	    
-	    foreach ($files_array as $file_name){
+	    foreach ($files_array as $file_name)
+        {
 	    	// чтение файла с заявкой
 	        $file = fopen($file_name, "r") or die("Unable to open file!");
 	        $file = fread($file,filesize($file_name));
 
 	        // преобразование строки в массив через символ | -> A|B|C = ['A','B','C']
 	        $file = explode("|", $file);
-
+            
 	        // проходится по email
-	        foreach ($firsthalf as $email){
-
-	            $mailfrom = 'MY_EMAIL_1@gmail.com';
-
-				$id = mb_convert_encoding($file[0], "UTF-8", "WINDOWS-1251");
-	            $subject = "Новая заявка по грузоперевозкам $id";
-
-	            // Формируется текст письма
-	            $message = "";
-	            foreach ($file as $value) {
-	                $message .= mb_convert_encoding($value, "UTF-8", "WINDOWS-1251")."\n\n";
-	            }
-	            
-	            // Настройка PHPMailer и SMTP
-	            $mail = new PHPMailer();
-	            $mail->IsSMTP();
-	            $mail->CharSet = "utf-8";
-	            $mail->Host = 'smtp.gmail.com';
-	            $mail->Port = 587;
-	            $mail->SMTPAuth = true;
-	            $mail->Username = 'haytek.server@gmail.com';
-	            $mail->Password = 'PASSWORD';
-	            $mail->SMTPSecure = 'tsl';
-
-	            $mail->From = $mailfrom;
-	            $mail->FromName = $mailfrom;
-	            $mail->AddAddress($newemail, "");
-
-	            // Указывается Email здесь, на данный момент берёт значение $email из массива со всеми E-mail'ами
-	            $mail->AddAddress(strtolower(trim($email)), "");
-
-	            $mail->AddReplyTo($mailfrom);
-
-	            $mail->Body = stripslashes($message);
-	            $mail->Subject = stripslashes($subject);
-	            $mail->WordWrap = 80;
-
-	            if (!$mail->Send())
-	            {
-	            	// Запись в лог файл сведения об ошибке
-	            	$log = fopen("log.txt","a");
-					fwrite($log, iconv("UTF-8", "WINDOWS-1251", "\n\n1 часть - Ошибка при отправке $email, \n\n".$mail->ErrorInfo.",  \n\n$message"));
-					fclose($log);
-	                continue;
-	            } else {sleep(2);}
-	            
-	        }// for each emails
-	 
-
-			foreach ($secondhalf as $email){
-
-	            $mailfrom = 'MY_EMAIL_2@gmail.com';
-
-				$id = mb_convert_encoding($file[0], "UTF-8", "WINDOWS-1251");
-	            $subject = "Новая заявка по грузоперевозкам $id";
-
-	            // Формируется текст письма
-	            $message = "";
-	            foreach ($file as $value) {
-	                $message .= mb_convert_encoding($value, "UTF-8", "WINDOWS-1251")."\n\n";
-	            }
-	            
-	            // Настройка PHPMailer и SMTP
-	            $mail = new PHPMailer();
-	            $mail->IsSMTP();
-	            $mail->CharSet = "utf-8";
-	            $mail->Host = 'smtp.gmail.com';
-	            $mail->Port = 587;
-	            $mail->SMTPAuth = true;
-	            $mail->Username = 'MY_EMAIL_2@gmail.com';
-	            $mail->Password = 'PASSWORD';
-	            $mail->SMTPSecure = 'tsl';
-
-	            $mail->From = $mailfrom;
-	            $mail->FromName = $mailfrom;
-	            $mail->AddAddress($newemail, "");
-
-	            // Указывается Email здесь, на данный момент берёт значение $email из массива со всеми E-mail'ами
-	            $mail->AddAddress(strtolower(trim($email)), "");
-
-	            $mail->AddReplyTo($mailfrom);
-
-	            $mail->Body = stripslashes($message);
-	            $mail->Subject = stripslashes($subject);
-	            $mail->WordWrap = 80;
-
-	            if (!$mail->Send())
-	            {
-	            	// Запись в лог файл сведения об ошибке
-	            	$log = fopen("log.txt","a");
-					fwrite($log, iconv("UTF-8", "WINDOWS-1251", "\n\n2 часть - Ошибка при отправке $email, \n\n".$mail->ErrorInfo.",  \n\n$message"));
-					fclose($log);
-	                continue;
-	            } else {sleep(2);}
-	            
-	        }// for each emails
+            mail_send($firsthalf, $file, 'MAIL_1', 'PASSWORD_1', 'SERVER_1', 'PORT_1');
+            mail_send($secondhalf, $file, 'MAIL_2', 'PASSWORD_2', 'SERVER_2', 'PORT_2');
+            mail_send($thirdhalf, $file, 'MAIL_3', 'PASSWORD_3', 'SERVER_3', 'PORT_3');
 
 	        unlink($file_name);
 
-	    }// for each files
+	    }
 
 	    $check = fopen("check.txt","w");
 		fwrite($check, "OFF");
